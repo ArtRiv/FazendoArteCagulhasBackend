eb58cea10a50d8190f87e6f17afdf6c55894b79d
unknown <arthurfelaco707@gmail.com>
2024-06-02 20:23:26 -0300

    feat: add pagination and sorting to results route

--- src/results/results.controller.ts
+++ src/results/results.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { Product } from '@prisma/client';
 import { ResultsService } from './results.service';
+import { SortByTypes } from 'src/types/product_sort_by';
 
 @Controller('results')
 export class ResultsController {
@@ -8,9 +9,15 @@ export class ResultsController {
 
   @Get()
   async getFilteredProducts(
-    @Query('search_query') searchQuery: string,
+    @Query('search_query') search_query: string,
+    @Query('page') page: number = 1,
+    @Query('sort_by') sort_by: SortByTypes = 'BEST_SELLING' as SortByTypes,
   ): Promise<Product[]> {
-    const results = await this.resultsService.getProductsBySearch(searchQuery);
+    const results = await this.resultsService.getProductsBySearch({
+      search_query,
+      page,
+      sort_by,
+    });
 
     return results;
   }
--- src/results/results.service.ts
+++ src/results/results.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
+import { ResultsProductQueryParams } from 'src/interfaces/query_params.interface';
+import { getPagination } from 'src/utils/getPagination';
+import { getSortByQueryConstraint } from 'src/utils/getSortByConstraint';
 
 @Injectable()
 export class ResultsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getProductsBySearch(searchQuery: string) {
+  async getProductsBySearch(params: ResultsProductQueryParams) {
+    const { skip, take } = getPagination(params.page);
+    const { field, order } = getSortByQueryConstraint(params.sort_by);
+
     const results = await this.prisma.product.findMany({
       where: {
-        tag: { search: searchQuery },
+        tag: { search: params.search_query },
       },
-      take: 4,
+      orderBy: {
+        [field]: order,
+      },
+      take: take,
+      skip: skip,
     });
 
-    console.log(results);
     return results;
   }
 }
