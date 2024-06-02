911d64d42be899bda873c2ed1da19c9968d20d92
unknown <arthurfelaco707@gmail.com>
2024-06-02 20:22:54 -0300

    Feat: add results route query params interface

--- src/interfaces/query_params.interface.ts
+++ src/interfaces/query_params.interface.ts
@@ -1,8 +1,14 @@
-import { ProductCategoriesType } from '../types/product_category';
+import { ProductCategoriesTypes } from '../types/product_category';
 import { SortByTypes } from '../types/product_sort_by';
 
-export interface ProductQueryParams {
-  page?: number;
-  category?: ProductCategoriesType;
-  sort_by?: SortByTypes;
+export interface FilteredProductQueryParams {
+  page: number;
+  category: ProductCategoriesTypes;
+  sort_by: SortByTypes;
+}
+
+export interface ResultsProductQueryParams {
+  search_query: string;
+  page: number;
+  sort_by: SortByTypes;
 }
