a68eeb06961262494bd38fdac780c92842f92d14
unknown <arthurfelaco707@gmail.com>
2024-06-02 22:58:05 -0300

    refactor: puts routes into a folder for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
-import { ResultsModule } from './results/results.module';
+import { ProductModule } from './routes/product/product.module';
+import { ReviewModule } from './routes/review/review.module';
+import { ResultsModule } from './routes/results/results.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ProductModule, ReviewModule, ResultsModule],
