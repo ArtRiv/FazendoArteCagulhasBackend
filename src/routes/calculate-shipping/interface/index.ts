c8e907ccfc70874f3c3d53298e18d8070c710924
ArtRiv <arthurfelaco707@gmail.com>
2024-09-05 18:27:43 -0300

    delete: idk

--- src/routes/calculate-shipping/interface/index.ts
+++
@@ -1,5 +0,0 @@
-import { PartialShippingOption } from "src/routes/calculate-shipping/interface/shipping_option.interface";
-
-export interface IShippingService {
-    calculateShipping( zipCode: string ): Promise<PartialShippingOption[]>;
-}
